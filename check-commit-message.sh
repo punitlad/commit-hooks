5e96b5072c157d97f7ccb97c7d125dbf119e22f6
Punit Lad <punitlad@gmail.com>
2021-05-01 22:44:33 -0400

    remove comment

--- check-commit-message.sh
+++ check-commit-message.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-MSG=$(cat .git/COMMIT_EDITMSG) # incoming argument value is .git/COMMIT_EDITMSG
 MATCHER=$1 # incoming argument value from pre-commit
 
+MSG=$(cat .git/COMMIT_EDITMSG)
 if ! echo $MSG | grep -E $MATCHER ;then
   echo "Your commit message must contain the matcher $MATCHER"
   exit 1
