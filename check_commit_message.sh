dac050c54ae6e6e8aec6412ff6cebb7aa60846bd
Punit Lad <punitlad@gmail.com>
2021-05-02 08:34:28 -0400

    add argument types with help message

--- check_commit_message.sh
+++ check_commit_message.sh
@@ -1,14 +1,21 @@
 #!/bin/bash
 
-if [ "$#" -ne 1 ]; then
-    echo "check_commit_message requires a single argument stating regular expression to match"
+if [ "$#" -ne 2 ]; then
+    echo "check_commit_message requires a two arguments stating type and regular expression"
+    echo "possible types: check"
     exit 1
 fi
 
-MATCHER=$1 # incoming argument value from pre-commit
+if [ "$1" == "check" ]; then
+    MATCHER=$2 # incoming argument value from pre-commit
 
-MSG=$(cat .git/COMMIT_EDITMSG)
-if ! echo $MSG | grep -E $MATCHER ;then
-  echo "Your commit message must contain the matcher $MATCHER"
-  exit 1
+    MSG=$(cat .git/COMMIT_EDITMSG)
+    if ! echo $MSG | grep -E $MATCHER ;then
+        echo "Your commit message must contain the matcher $MATCHER"
+        exit 1
+    fi
+else 
+    echo "check_commit_message requires a two arguments stating type and regular expression"
+    echo "possible types: check"
+    exit 1
 fi
