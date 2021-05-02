aa6d851c0336add83cf34c3916747087279af132
Punit Lad <punitlad@gmail.com>
2021-05-01 22:25:31 -0400

    check git-commit message when testing message

--- check-commit-message.sh
+++ check-commit-message.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-if ! echo "$1" | grep -E "test";then
+MSG=$(cat $1) # incoming argument value is .git/COMMIT_EDITMSG
+
+if ! echo $MSG | grep -E "test";then
   echo "Your commit message must contain the word test"
   exit 1
 fi
-
